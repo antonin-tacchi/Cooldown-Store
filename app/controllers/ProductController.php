0fc71911a6d4f1f812de851d4f44eff26aaad670
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-05-11 18:52:35 +0200

    chore(ci): remove PHP setup and related steps from CI workflow

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -3,6 +3,7 @@
 // Ajout d'une fonction minimale pour éviter les erreurs de code vide lors de l'analyse avec PHP_CodeSniffer (phpcs).
 // Cette fonction ne fait rien mais permet de valider les fichiers sans logique métier.
 
-function exampleFunctionCI() { 
+function exampleFunctionCI()
+{
     return true;
 }
