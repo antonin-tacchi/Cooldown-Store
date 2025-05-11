a78276c648b7897d671075790aa2ef4609511c5a
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-05-11 22:29:30 +0200

    chore(ci, config): updates ESLint and GitHub Actions configuration for linting and automatic corrections

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,31 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'indent': ['error', 4],
+      'quotes': ['error', 'single'],  // Utiliser des guillemets simples
+      'semi': ['error', 'always'],
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+    files: ['**/*.mjs', '**/*.cjs'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],  // Utiliser des guillemets simples
+      'semi': ['error', 'always'],
+    },
+  },
+];
