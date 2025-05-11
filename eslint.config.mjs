bfc0895309443fbfd1a4edb81b611cc6a0f304ae
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-05-11 22:57:36 +0200

    chore(eslint): fix config by removing unsupported structuredClone global

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,6 @@ export default [
       sourceType: 'module',
       globals: {
         ...globals.browser,
-        structuredClone: 'readonly',
       },
     },
     rules: {
@@ -26,7 +25,6 @@ export default [
       sourceType: 'module',
       globals: {
         ...globals.node,
-        structuredClone: 'readonly',
       },
     },
     rules: {
